7e97fa7183f68349644ca8742b30ba3ea75c1fc2
Adam Clayman <adam@clayman.org>
2017-09-27 18:00:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ function divide(x, y) {
 }
 
 function inc(n) {
-  n += 1;
+  return n += 1;
 }
 
 function dec(n) {
-  n -= 1;
+  return n -= 1;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
 }
